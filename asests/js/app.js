be13ac350b4ea528d74ae7a51161f2883fa18cff
sebastiansevsorcia@gmail.com <sebastiansevsorcia@gmail.com>
2022-05-15 11:11:24 -0500

    pus

--- asests/js/app.js
+++ asests/js/app.js
@@ -13,7 +13,6 @@ function menu(){
 };
 
 const links = document.querySelectorAll(".link");
-
 for(link of links){
   link.addEventListener("click",(e)=>{
   /*  e.preventDefault(); */
@@ -23,7 +22,6 @@ for(link of links){
     navMenu.classList.remove("header-menu-active");
   });
 };
-
   
 let images = document.querySelectorAll(".card-image img");
 let boxModal = document.querySelector(".box-modal");
@@ -50,9 +48,52 @@ for (let image of images){
 
 /* scrollReval */
 window.sr = new ScrollReveal();
-
 sr.reveal('section', {
   delay: 500,
   interval: 16, 
   reset: true
 });
+
+/* validación de formulario */
+
+let form = document.getElementById("form-contac");
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+let message = document.getElementById("message");
+var numbersRegex  = /^[A-Z]+$/i;
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault();
+  let errors = document.getElementById("errors");
+  let warning = "";
+  let validar = false;
+  errors.innerHTML = "";
+  
+  if(name.value.length == ""){
+    warning += `el nombre no debe estar vacío <br>`;
+    validar = true;
+  }
+  else if(name.value.length < 12){
+    warning += `El nombre es corto <br>`;
+    validar = true;
+  }
+  if(!emailRegex.test(email.value)){
+    warning += `el email no es válido <br>`;
+    validar = true;
+  }
+  if(message.value.length == ""){
+    warning +=`El mensaje no puede estar vacío <br>`;
+    validar = true;
+  }
+  else if(message.value.length < 45){
+    warning += `El mensaje es corto <br>`;
+    validar = true;
+  }
+  if(validar){
+    errors.innerHTML = warning;
+  }else{
+    errors.innerHTML = "El formulario se ha enviado...";
+  }
+})
+
